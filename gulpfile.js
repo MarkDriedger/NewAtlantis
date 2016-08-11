a9d5bbfaf3ae46e3f7ffa45c2805f628c1dd2f00
Mark Driedger <mark@markdriedger.com>
2016-08-10 20:38:44 -0400

    added html & css files and a server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var vinylSourceStream = require('vinyl-source-stream');
+var connect = require('gulp-connect');
 
 gulp.task('browserify',function(){
   return browserify('./src/js/app.js')
@@ -10,7 +11,25 @@ gulp.task('browserify',function(){
 });
 
 gulp.task('watch',function(){
-  gulp.watch('./src/js/*.js', ['browserify'])
+  gulp.watch('./src/js/*.js', ['browserify']);
+  gulp.watch('./src/html/*.html', ['html']);
+  gulp.watch('./src/css/*.css', ['css']);
+});
+
+gulp.task('html',function(){
+  return gulp.src('./src/html/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('css',function(){
+  return gulp.src('./src/css/*.css')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('serve',function(){
+  connect.server({root:['build']});
 })
 
-gulp.task('default',['browserify','watch']);
+
+
+gulp.task('default',['browserify','watch','html','css','serve']);
